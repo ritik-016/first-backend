4d27902b78fa382ad74d0166da3442edd42b93d2
Ritik Kumar <ad6360153@gmail.com>
2025-05-16 19:40:49 +0530

    utils code

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,8 @@
+class apiResponse {
+    constructor(statusCode, message, data = "Success") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+    }
+}
