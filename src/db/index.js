fd8363173d38279efe45b6e53151e8795e1ba31f
Ritik Kumar <ad6360153@gmail.com>
2025-05-17 22:50:21 +0530

    JWT token

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log(` MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.log("Failed connecting to MongoDB:", error);
     process.exit(1);
